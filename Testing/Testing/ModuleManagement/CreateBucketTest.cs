328fd0cfd658cc0c6c335c4966183d5640dd2548
Trang Nguyen <trangnguyen2637@gmail.com>
2019-04-10 21:32:42 +0700

    fixed

--- Testing/Testing/ModuleManagement/CreateBucketTest.cs
+++ Testing/Testing/ModuleManagement/CreateBucketTest.cs
@@ -15,11 +15,11 @@ namespace Testing
     {
         [Test]
         [TestCaseSource(typeof(TestBase), "BrowserToRunWith")]
-        public void TC_FUC_MO02_09(String browserName)
+        public void TC_FUC_MO02_10(String browserName)
         {
             SetUp(browserName);
             //throw new NotFiniteNumberException();
-
+            test = extent.StartTest("CREATE BUCKET TC_FUC_MO02_10");
             IWebElement email = driver.FindElement(By.CssSelector("#loginForm > form > div.wrap-input100.rs1.validate-input > input"));
             email.SendKeys("trangnguyen237@vanlanguni.vn");
             IWebElement pass = driver.FindElement(By.CssSelector("#loginForm > form > div.wrap-input100.rs2.validate-input > input"));
@@ -27,48 +27,26 @@ namespace Testing
             IWebElement login = driver.FindElement(By.CssSelector("#loginForm > form > div.container-login100-form-btn > button"));
             login.Click();
             //Select plan
-            IWebElement plan = driver.FindElement(By.XPath("/html/body/div/div[1]/section[2]/div[2]/div/div[2]/table/tbody/tr[2]"));
+            IWebElement plan = driver.FindElement(By.CssSelector("body > div > div.content-wrapper > section.content > div.col-xs-12 > div > div.box-body.table-responsive.no-padding > table > tbody > tr:nth-child(2) > td:nth-child(2) > a"));
             plan.Click();
+            test.Log(LogStatus.Pass, "Select plan name you want to create bucket");
+            test.Log(LogStatus.Pass, "System open Management page");
             //Create bucket
             IWebElement newbucket = driver.FindElement(By.CssSelector("body > section.lists-container > button"));
             newbucket.Click();
+            test.Log(LogStatus.Pass, "At Plan Choose 'Add a bucket'");
             //
             IWebElement namebucket = driver.FindElement(By.CssSelector("#Title"));
             namebucket.Click();
             IWebElement name = driver.FindElement(By.CssSelector("#Title"));
             name.SendKeys("test create bucket");
+            test.Log(LogStatus.Pass, "Enter valid value into 'Bucket Name sendkey : test create bucket");
             IWebElement create = driver.FindElement(By.CssSelector("#myModal > div > div > div.modal-body > form > div.modal-footer > input"));
             create.Click();
+            test.Log(LogStatus.Pass, "Click 'Create' button and observe the displayed result");
             //
-            test = extent.StartTest("TC_FUC_MO02_09");
             Assert.IsTrue(true);
             test.Log(LogStatus.Pass, "Assert Pass as condition is True");
-        }
-
-        [Test]
-        [TestCaseSource(typeof(TestBase), "BrowserToRunWith")]
-        public void TC_FUC_MO02_10(String browserName)
-        {
-            SetUp(browserName);
-            //throw new NotFiniteNumberException();
-
-            IWebElement email = driver.FindElement(By.CssSelector("#loginForm > form > div.wrap-input100.rs1.validate-input > input"));
-            email.SendKeys("trangnguyen237@vanlanguni.vn");
-            IWebElement pass = driver.FindElement(By.CssSelector("#loginForm > form > div.wrap-input100.rs2.validate-input > input"));
-            pass.SendKeys("1234567890");
-            IWebElement login = driver.FindElement(By.CssSelector("#loginForm > form > div.container-login100-form-btn > button"));
-            login.Click();
-            //Select plan
-            IWebElement plan = driver.FindElement(By.CssSelector("body > div > div.content-wrapper > section.content > div:nth-child(4) > div > div.box-body.table-responsive.no-padding > table > tbody > tr:nth-child(2) > td:nth-child(2) > a"));
-            plan.Click();
-            IWebElement newbucket = driver.FindElement(By.CssSelector("body > section.lists-container > button"));
-            newbucket.Click();
-            newbucket.SendKeys("");
-            IWebElement create = driver.FindElement(By.CssSelector("#myModal > div > div > div.modal-body > form > div.modal-footer > input"));
-            create.Click();
-            test = extent.StartTest("TC_FUC_MO02_10");
-            Assert.IsTrue(true);
-            test.Log(LogStatus.Pass, "Invalid name bucket attempt");
         }
 
         [Test]
@@ -77,23 +55,34 @@ namespace Testing
         {
             SetUp(browserName);
             //throw new NotFiniteNumberException();
-
+            test = extent.StartTest("CREATE BUCKET TC_FUC_MO02_11");
             IWebElement email = driver.FindElement(By.CssSelector("#loginForm > form > div.wrap-input100.rs1.validate-input > input"));
             email.SendKeys("trangnguyen237@vanlanguni.vn");
             IWebElement pass = driver.FindElement(By.CssSelector("#loginForm > form > div.wrap-input100.rs2.validate-input > input"));
             pass.SendKeys("1234567890");
             IWebElement login = driver.FindElement(By.CssSelector("#loginForm > form > div.container-login100-form-btn > button"));
             login.Click();
-            IWebElement plan = driver.FindElement(By.CssSelector("body > div > div.content-wrapper > section.content > div:nth-child(4) > div > div.box-body.table-responsive.no-padding > table > tbody > tr:nth-child(3) > td:nth-child(2) > a"));
+            //Select plan
+            IWebElement plan = driver.FindElement(By.CssSelector("body > div > div.content-wrapper > section.content > div.col-xs-12 > div > div.box-body.table-responsive.no-padding > table > tbody > tr:nth-child(2) > td:nth-child(2) > a"));
             plan.Click();
+            test.Log(LogStatus.Pass, "Select plan name you want to create bucket");
+            test.Log(LogStatus.Pass, "System open Management page");
+            //Create bucket
             IWebElement newbucket = driver.FindElement(By.CssSelector("body > section.lists-container > button"));
             newbucket.Click();
-            newbucket.SendKeys("%%%%%&&");
+            test.Log(LogStatus.Pass, "At Plan Choose 'Add a bucket'");
+            //
+            IWebElement namebucket = driver.FindElement(By.CssSelector("#Title"));
+            namebucket.Click();
+            IWebElement name = driver.FindElement(By.CssSelector("#Title"));
+            name.SendKeys("");
+            test.Log(LogStatus.Pass, "Leave blank 'Bucket Name'");
             IWebElement create = driver.FindElement(By.CssSelector("#myModal > div > div > div.modal-body > form > div.modal-footer > input"));
             create.Click();
-            test = extent.StartTest("TC_FUC_MO02_11");
+            test.Log(LogStatus.Pass, "Click 'Create' button and observe the displayed result");
+            //
             Assert.IsTrue(true);
-            test.Log(LogStatus.Pass, "Invalid name bucket attempt");
+            test.Log(LogStatus.Warning, "Invalid name bucket attempt");
         }
 
         [Test]
@@ -102,7 +91,7 @@ namespace Testing
         {
             SetUp(browserName);
             //throw new NotFiniteNumberException();
-
+            test = extent.StartTest("CREATE BUCKET TC_FUC_MO02_12");
             IWebElement email = driver.FindElement(By.CssSelector("#loginForm > form > div.wrap-input100.rs1.validate-input > input"));
             email.SendKeys("trangnguyen237@vanlanguni.vn");
             IWebElement pass = driver.FindElement(By.CssSelector("#loginForm > form > div.wrap-input100.rs2.validate-input > input"));
@@ -110,22 +99,62 @@ namespace Testing
             IWebElement login = driver.FindElement(By.CssSelector("#loginForm > form > div.container-login100-form-btn > button"));
             login.Click();
             //Select plan
-            IWebElement plan = driver.FindElement(By.XPath("/html/body/div/div[1]/section[2]/div[2]/div/div[2]/table/tbody/tr[2]"));
+            IWebElement plan = driver.FindElement(By.CssSelector("body > div > div.content-wrapper > section.content > div.col-xs-12 > div > div.box-body.table-responsive.no-padding > table > tbody > tr:nth-child(2) > td:nth-child(2) > a"));
             plan.Click();
+            test.Log(LogStatus.Pass, "Select plan name you want to create bucket");
+            test.Log(LogStatus.Pass, "System open Management page");
             //Create bucket
             IWebElement newbucket = driver.FindElement(By.CssSelector("body > section.lists-container > button"));
             newbucket.Click();
+            test.Log(LogStatus.Pass, "At Plan Choose 'Add a bucket'");
+            //
+            IWebElement namebucket = driver.FindElement(By.CssSelector("#Title"));
+            namebucket.Click();
+            IWebElement name = driver.FindElement(By.CssSelector("#Title"));
+            name.SendKeys("%%%%%&&");
+            test.Log(LogStatus.Pass, "Input special characters 'Bucket Name' sendkey : %%%%%&&");
+            IWebElement create = driver.FindElement(By.CssSelector("#myModal > div > div > div.modal-body > form > div.modal-footer > input"));
+            create.Click();
+            test.Log(LogStatus.Pass, "Click 'Create' button and observe the displayed result");
+            //
+            Assert.IsTrue(true);
+            test.Log(LogStatus.Warning, "Invalid name bucket attempt");
+        }
+
+        [Test]
+        [TestCaseSource(typeof(TestBase), "BrowserToRunWith")]
+        public void TC_FUC_MO02_13(String browserName)
+        {
+            SetUp(browserName);
+            //throw new NotFiniteNumberException();
+            test = extent.StartTest("CREATE BUCKET TC_FUC_MO02_13");
+            IWebElement email = driver.FindElement(By.CssSelector("#loginForm > form > div.wrap-input100.rs1.validate-input > input"));
+            email.SendKeys("trangnguyen237@vanlanguni.vn");
+            IWebElement pass = driver.FindElement(By.CssSelector("#loginForm > form > div.wrap-input100.rs2.validate-input > input"));
+            pass.SendKeys("1234567890");
+            IWebElement login = driver.FindElement(By.CssSelector("#loginForm > form > div.container-login100-form-btn > button"));
+            login.Click();
+            //Select plan
+            IWebElement plan = driver.FindElement(By.CssSelector("body > div > div.content-wrapper > section.content > div.col-xs-12 > div > div.box-body.table-responsive.no-padding > table > tbody > tr:nth-child(2) > td:nth-child(2) > a"));
+            plan.Click();
+            test.Log(LogStatus.Pass, "Select plan name you want to create bucket");
+            test.Log(LogStatus.Pass, "System open Management page");
+            //Create bucket
+            IWebElement newbucket = driver.FindElement(By.CssSelector("body > section.lists-container > button"));
+            newbucket.Click();
+            test.Log(LogStatus.Pass, "At Plan Choose 'Add a bucket'");
             //
             IWebElement namebucket = driver.FindElement(By.CssSelector("#Title"));
             namebucket.Click();
             IWebElement name = driver.FindElement(By.CssSelector("#Title"));
             name.SendKeys("test create bucket");
+            test.Log(LogStatus.Pass, "Enter valid value into 'Bucket Name sendkey : test create bucket");
             IWebElement create = driver.FindElement(By.CssSelector("#myModal > div > div > div.modal-body > form > div.modal-footer > input"));
             create.Click();
+            test.Log(LogStatus.Pass, "Click 'Create' button and observe the displayed result");
             //
-            test = extent.StartTest("TC_FUC_MO02_12");
             Assert.IsTrue(true);
-            test.Log(LogStatus.Pass, "Assert Pass as condition is True");
+            test.Log(LogStatus.Warning, "Invalid name bucket attempt");
         }
 
     }
--- Testing/Testing/ModuleManagement/CreateTaskTest.cs
+++ Testing/Testing/ModuleManagement/CreateTaskTest.cs
@@ -15,11 +15,11 @@ namespace Testing.ModuleManagement
     {
         [Test]
         [TestCaseSource(typeof(TestBase), "BrowserToRunWith")]
-        public void TC_FUC_MO02_13(String browserName)
+        public void TC_FUC_MO02_15(String browserName)
         {
             SetUp(browserName);
             //throw new NotFiniteNumberException();
-
+            test = extent.StartTest("CREATE TASK TC_FUC_MO02_15");
             IWebElement email = driver.FindElement(By.CssSelector("#loginForm > form > div.wrap-input100.rs1.validate-input > input"));
             email.SendKeys("trangnguyen237@vanlanguni.vn");
             IWebElement pass = driver.FindElement(By.CssSelector("#loginForm > form > div.wrap-input100.rs2.validate-input > input"));
@@ -27,74 +27,89 @@ namespace Testing.ModuleManagement
             IWebElement login = driver.FindElement(By.CssSelector("#loginForm > form > div.container-login100-form-btn > button"));
             login.Click();
             //Select plan
-            IWebElement plan = driver.FindElement(By.CssSelector("body > div > div.content-wrapper > section.content > div:nth-child(4) > div > div.box-body.table-responsive.no-padding > table > tbody > tr:nth-child(2) > td:nth-child(2) > a"));
+            IWebElement plan = driver.FindElement(By.CssSelector("body > div > div.content-wrapper > section.content > div.col-xs-12 > div > div.box-body.table-responsive.no-padding > table > tbody > tr:nth-child(2) > td:nth-child(2) > a"));
             plan.Click();
+            test.Log(LogStatus.Pass, "Open Management page");
             //Create task
-            IWebElement newtask = driver.FindElement(By.CssSelector("body > section.lists-container > div:nth-child(1) > button"));
+            IWebElement newtask = driver.FindElement(By.CssSelector("body > section.lists-container > div.list > button"));
             newtask.Click();
-            //
-            /*IWebElement namebucket = driver.FindElement(By.CssSelector("#Title"));
-            namebucket.Click();
-            IWebElement name = driver.FindElement(By.CssSelector("#Title"));
-            name.SendKeys("test create task");
-            IWebElement create = driver.FindElement(By.CssSelector("#myModalll61 > div > div > div.modal-body > form > div:nth-child(15) > div > input"));
-            create.Click();*/
-            //
-            test = extent.StartTest("TC_FUC_MO02_13");
+            test.Log(LogStatus.Pass, "Select 'Add a task' at name bucket want create task");
+            IWebElement clicknametask = driver.FindElement(By.CssSelector("#Title"));
+            clicknametask.Click();
+            IWebElement nametask = driver.FindElement(By.CssSelector("#Title"));
+            nametask.SendKeys("Test Create Task");
+            test.Log(LogStatus.Pass, "Enter valid value into 'Task Name' sendkey : Test Create Task");
+            IWebElement create = driver.FindElement(By.CssSelector("#myModalll11 > div > div > div.modal-body > form > div:nth-child(15) > div > input"));
+            create.Click();
+            test.Log(LogStatus.Pass, "Click 'Create' button and observe the displayed result");
             Assert.IsTrue(true);
             test.Log(LogStatus.Pass, "Assert Pass as condition is True");
         }
 
         [Test]
         [TestCaseSource(typeof(TestBase), "BrowserToRunWith")]
-        public void TC_FUC_MO02_14(String browserName)
+        public void TC_FUC_MO02_16(String browserName)
         {
             SetUp(browserName);
             //throw new NotFiniteNumberException();
-
+            test = extent.StartTest("CREATE TASK TC_FUC_MO02_16");
             IWebElement email = driver.FindElement(By.CssSelector("#loginForm > form > div.wrap-input100.rs1.validate-input > input"));
             email.SendKeys("trangnguyen237@vanlanguni.vn");
             IWebElement pass = driver.FindElement(By.CssSelector("#loginForm > form > div.wrap-input100.rs2.validate-input > input"));
             pass.SendKeys("1234567890");
             IWebElement login = driver.FindElement(By.CssSelector("#loginForm > form > div.container-login100-form-btn > button"));
             login.Click();
-            IWebElement plan = driver.FindElement(By.CssSelector("body > div > div.content-wrapper > section.content > div:nth-child(4) > div > div.box-body.table-responsive.no-padding > table > tbody > tr:nth-child(3) > td:nth-child(2) > a"));
+            //Select plan
+            IWebElement plan = driver.FindElement(By.CssSelector("body > div > div.content-wrapper > section.content > div.col-xs-12 > div > div.box-body.table-responsive.no-padding > table > tbody > tr:nth-child(2) > td:nth-child(2) > a"));
             plan.Click();
-            IWebElement task = driver.FindElement(By.CssSelector("body > section.lists-container > div.list > button"));
-            task.Click();
-            IWebElement newtask = driver.FindElement(By.CssSelector("#Title"));
-            newtask.SendKeys("");
-            IWebElement create = driver.FindElement(By.CssSelector("#myModalll > div > div > div.modal-body > form > div:nth-child(15) > div > input"));
+            test.Log(LogStatus.Pass, "Open Management page");
+            //Create task
+            IWebElement newtask = driver.FindElement(By.CssSelector("body > section.lists-container > div.list > button"));
+            newtask.Click();
+            test.Log(LogStatus.Pass, "Select 'Add a task' at name bucket want create task");
+            IWebElement clicknametask = driver.FindElement(By.CssSelector("#Title"));
+            clicknametask.Click();
+            IWebElement nametask = driver.FindElement(By.CssSelector("#Title"));
+            nametask.SendKeys("");
+            test.Log(LogStatus.Pass, "Leave blank 'Task Name'");
+            IWebElement create = driver.FindElement(By.CssSelector("#myModalll11 > div > div > div.modal-body > form > div:nth-child(15) > div > input"));
             create.Click();
-            test = extent.StartTest("TC_FUC_MO02_13");
+            test.Log(LogStatus.Pass, "Click 'Create' button and observe the displayed result");
             Assert.IsTrue(true);
-            test.Log(LogStatus.Pass, "Invalid name task attempt");
+            test.Log(LogStatus.Warning, "Invalid name task attempt");
         }
 
         [Test]
         [TestCaseSource(typeof(TestBase), "BrowserToRunWith")]
-        public void TC_FUC_MO02_15(String browserName)
+        public void TC_FUC_MO02_17(String browserName)
         {
             SetUp(browserName);
             //throw new NotFiniteNumberException();
-
+            test = extent.StartTest("CREATE TASK TC_FUC_MO02_17");
             IWebElement email = driver.FindElement(By.CssSelector("#loginForm > form > div.wrap-input100.rs1.validate-input > input"));
             email.SendKeys("trangnguyen237@vanlanguni.vn");
             IWebElement pass = driver.FindElement(By.CssSelector("#loginForm > form > div.wrap-input100.rs2.validate-input > input"));
             pass.SendKeys("1234567890");
             IWebElement login = driver.FindElement(By.CssSelector("#loginForm > form > div.container-login100-form-btn > button"));
             login.Click();
-            IWebElement plan = driver.FindElement(By.CssSelector("body > div > div.content-wrapper > section.content > div:nth-child(4) > div > div.box-body.table-responsive.no-padding > table > tbody > tr:nth-child(3) > td:nth-child(2) > a"));
+            //Select plan
+            IWebElement plan = driver.FindElement(By.CssSelector("body > div > div.content-wrapper > section.content > div.col-xs-12 > div > div.box-body.table-responsive.no-padding > table > tbody > tr:nth-child(2) > td:nth-child(2) > a"));
             plan.Click();
-            IWebElement task = driver.FindElement(By.CssSelector("body > section.lists-container > div.list > button"));
-            task.Click();
-            IWebElement newtask = driver.FindElement(By.CssSelector("#Title"));
-            newtask.SendKeys("***");
-            IWebElement create = driver.FindElement(By.CssSelector("#myModalll > div > div > div.modal-body > form > div:nth-child(15) > div > input"));
+            test.Log(LogStatus.Pass, "Open Management page");
+            //Create task
+            IWebElement newtask = driver.FindElement(By.CssSelector("body > section.lists-container > div.list > button"));
+            newtask.Click();
+            test.Log(LogStatus.Pass, "Select 'Add a task' at name bucket want create task");
+            IWebElement clicknametask = driver.FindElement(By.CssSelector("#Title"));
+            clicknametask.Click();
+            IWebElement nametask = driver.FindElement(By.CssSelector("#Title"));
+            nametask.SendKeys("Test Create Task");
+            test.Log(LogStatus.Pass, "Input special characters 'Task Name' sendkey : ***");
+            IWebElement create = driver.FindElement(By.CssSelector("#myModalll11 > div > div > div.modal-body > form > div:nth-child(15) > div > input"));
             create.Click();
-            test = extent.StartTest("TC_FUC_MO02_13");
+            test.Log(LogStatus.Pass, "Click 'Create' button and observe the displayed result");
             Assert.IsTrue(true);
-            test.Log(LogStatus.Pass, "Invalid name task attempt");
+            test.Log(LogStatus.Warning, "Invalid name task attempt");
         }
     }
 }
